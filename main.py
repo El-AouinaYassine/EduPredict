c766f4e8fe42f39aecbd7df7ea9c725b6ae692dd
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2024-12-03 00:08:07 +0100

    approxt

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt 
+def getW(path):
+    y , x = np.loadtxt(path , skiprows=1 , unpack = True)
+    
+    y= np.array(y)
+    x= np.array(x)
+
+    all = y/x
+    res = 0 
+    for item in all :
+        res += item
+    return res
+
+def predict(res , w):
+    return res * w 
+
+# plt.xlabel("x")
+# plt.ylabel("y")
+
+
 
 sns.set();
 plt.axis([0,100 , 0,100])
@@ -19,12 +38,9 @@ plt.plot(X , Y , "bo")
 # the reg line
 x= np.linspace(0 , 10000)
 w = 2
-y=x*w
+y=x * getW("pizza.txt")
 sns.lineplot(y=y, x=x)
 plt.title(f"y = x")
-# plt.xlabel("x")
-# plt.ylabel("y")
 
-def predict(res , w):
-    return res * w 
+# print(predict(20, 2))
 plt.show()
