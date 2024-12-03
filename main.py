46f1d029b07d67941a4012121f138fac6569ef5b
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2024-12-03 01:19:41 +0100

    finally solve the W

--- main.py
+++ main.py
@@ -16,28 +16,39 @@ def getW(path):
 def predict(res , w):
     return res * w 
 
-# plt.xlabel("x")
-# plt.ylabel("y")
+def loss(X, Y , w):
+    return np.average(predict(X, w) - Y)**2
+
+def train(X , Y , iterations , lr=0.01):
+    w = 0 
+    for i in range(iterations):
+        curr_loss = loss(X, Y, w)
+        print("Iteration %4d => Loss: %.6f" % (i, curr_loss))
+        if curr_loss > loss(X , Y , w+lr):
+            w+=lr
+        elif curr_loss > loss(X , Y , w-lr):
+            w-=lr
+        else:
+            return w
+    raise Exception("Couldn't converge within %d iterations" % iterations)
+
+  
 
 
 
 sns.set();
 plt.axis([0,100 , 0,100])
-
 plt.xticks(fontsize = 10)
 plt.yticks(fontsize = 10)
-
 plt.xlabel("Reservations" , fontsize=20)
 plt.ylabel("Pizzas" , fontsize =20)
-
 X,Y = np.loadtxt("pizza.txt" , skiprows = 1, unpack =True)
-
 plt.plot(X , Y , "bo")
 plt.plot(X , Y , "bo")
     
 # the reg line
 x= np.linspace(0 , 10000)
-w = 2
+w = train(X , Y , 100000 , 0.01)
 y=x * getW("pizza.txt")
 sns.lineplot(y=y, x=x)
 plt.title(f"y = x")
