8b806fd12465ff2069c5bd2acc0b0cacf08d3337
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2024-12-15 13:30:25 +0100

    grad descent

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ def predict(res , w , b):
     return res * w + b  
 
 def loss(X, Y , w , b):
-    return np.average((predict(X, w , b) - Y)**2)
+    return np.average((predict(X, w , b) - Y)**2) 
 
-def train(X , Y , iterations , lr=0.01):
+def gradient(X , Y , w ,b=0):
+    return 2 * np.average(X * (predict(X,w,b)-Y) )
+
+def train(X , Y , iterations , lr=0.01 ):
     w = b = 0 
+    tab = [];
     for i in range(iterations):
+        tab.append(int(gradient(X,Y,w,0)))
         curr_loss = loss(X, Y, w ,b)
-        print("Iteration %4d => Loss: %.6f (b calue : %.6f)" % (i, curr_loss , b))
+        # print("Iteration %4d => Loss: %.6f (b calue : %.6f)" % (i, curr_loss , b))
         if curr_loss > loss(X , Y , w+lr , b):
         # ila zdna +lr w kan loss jdid 7sn mn dyal db (sghr mno) 
             w+=lr # zid +lr l weigth
@@ -31,8 +36,19 @@ def train(X , Y , iterations , lr=0.01):
             b-=lr    
         else:
         # mor matsali zada wn9san wywli curr_loss mkytbdlsh return w
-            return w , b
+            return w , b ,tab
     raise Exception("Couldn't converge within %d iterations" % iterations)
+
+def train_gradiant(X,Y,iterations , lr = 0.01):
+    w = 0
+    b=0
+    for i in range(iterations):
+        print(f"iteration : {iterations}")
+        print(f"w:{w}")
+        w-=gradient(X,Y,w,b) *lr
+        
+    return w
+
 
 sns.set();
 plt.axis([0,100 , 0,100])
@@ -43,20 +59,17 @@ plt.ylabel("Pizzas" , fontsize =20)
 # skiprows => n9z str dyal lktaba li fih "reservation pizza"
 X,Y = np.loadtxt("score.txt" , skiprows = 1, unpack =True)
 plt.plot(X , Y , "bo")
-# plt.plot(X , Y , "bo")
     
 # the reg linewlahila malk 
 
 x= np.linspace(0 , 10000)
-w , b= train(X , Y , 1000000 , 0.01)
-y=x * w + b
+tab = []
+# w , b,tab= train(X , Y , 1000000 , 0.01)
+# w = 1;
+# b=0;
+y=x * train_gradiant(X,Y,10) + 2
 sns.lineplot(y=y, x=x)
 plt.title(f"y = x")
 
-
-# wv = predict(15 , w.12269999985915,b )
-print(f"wieght => {w}")
-
-# print(predict(20, 2))
 plt.grid(True)
 plt.show()
