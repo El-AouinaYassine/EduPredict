75568ae7a8bc40298842f688640ffceafa7bab84
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2025-01-14 22:22:05 +0100

    checkout

--- data_filtred/filter.py
+++ data_filtred/filter.py
@@ -1,5 +1,5 @@
 import numpy as np 
+import pandas as pd
 
-X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11,X12,X13,X14,Y1,Y2 = np.loadtxt("fff.txt" , skiprows=1 , unpack=True) #load data
-print(X1)
-
+data = pd.read_csv('DATASET.csv')
+print(data.head)
