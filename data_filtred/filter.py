8d440ae44adf89c4d70684d87a5de1988b957cb4
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2025-01-15 00:01:29 +0100

    presenting_data

--- data_filtred/filter.py
+++ data_filtred/filter.py
@@ -1,5 +1,19 @@
 import numpy as np 
 import pandas as pd
-
-data = pd.read_csv('DATASET.csv')
-print(data.head)
+data = pd.read_csv(
+    'DATASET.csv',
+    delimiter=',',
+    quotechar='"',
+    skipinitialspace=True,  # Skip spaces after delimiters
+    engine='python',       
+    )
+np_data = data.to_numpy()
+col_names = (data.columns).to_numpy()
+i = 0
+for item in np_data:
+    i+=1
+    print("\033[31mitem(%d):\033[0m" % i)
+    j=0
+    for col_val in item:
+        print("\033[32m\t(%s):{%s}\033[0m"%(col_names[j] , col_val))
+        j+=1
