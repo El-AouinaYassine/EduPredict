1b7400f8112d85bc6c7048572992fda1d31d1a37
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2025-01-24 08:08:32 +0100

    orginaze folders

--- data_filtred/filter.py
+++ data_filtred/filter.py
@@ -1,13 +1,24 @@
 import numpy as np 
 import pandas as pd
 data = pd.read_csv(
-    'DATASET.csv',
+    'catData.csv',
     delimiter=',',
     quotechar='"',
-    skipinitialspace=True,  # Skip spaces after delimiters
+    skipinitialspace=True, 
     engine='python',       
     )
-np_data = data.to_numpy()
+data.columns = data.columns.str.strip()
+
+def convert_cat(col , merged):
+    dummie = pd.get_dummies(data[col])
+    dummie = dummie.astype(int)
+    merged  = pd.concat([data,dummie] , axis='columns')
+    merged.drop([col] , axis='columns' , inplace=True)
+
+# convert_cat('Ville' ,  data)
+print(data["Ville"])
+# np_data = data.to_numpy()
+
 def show_data():
     col_names = (data.columns).to_numpy()
     i = 0
@@ -18,4 +29,4 @@ def show_data():
         for col_val in item:
             print("\033[32m\t(%s):\033[34m{%s} \033[0m\033[0m"%(col_names[j] , col_val))
             j+=1
-show_data()
+# show_data()
