c36930f5dfa5bf0740288270f366d51ab1a8b64c
El-AouinaYassine <yassine.elaouina1@usmba.ac.ma>
2025-02-14 13:32:08 +0100

    ff

--- interface/backend/server.js
+++ interface/backend/server.js
@@ -25,13 +25,13 @@ const CSV_HEADERS = [
 
 const MAPPINGS = {
     sexe: { "0": "Homme", "1": "Femme" },
-    niveau_anglais: { "1": "Débutant", "2": "Intermédiaire", "3": "Avancé", "4": "Fluent" },
-    niveau_francais: { "1": "Débutant", "2": "Intermédiaire", "3": "Avancé", "4": "Fluent" },
-    matiere_detestee: { "0": "None", "1": "Mathématiques", "2": "Physique", "3": "Chimie", "4": "Littérature", "5": "Histoire", "6": "Langues", "7": "Informatique", "8": "Biologie" },
-    loisirs: { "1": "Lecture", "2": "Sport", "3": "Musique", "4": "Voyage", "5": "Cinéma", "6": "Jeux vidéo", "7": "Arts plastiques", "8": "Bénévolat", "9": "Technologie", "10": "Ecriture", "11": "Photographie" },
-    matiere_preferee: { "1": "Mathématiques", "2": "Physique", "3": "Chimie", "4": "Littérature", "5": "Histoire", "6": "Langues", "7": "Informatique", "8": "Biologie" },
-    soft_skills: { "1": "Adaptabilité", "2": "Créativité", "3": "Résolution de problèmes", "4": "Autonomie", "5": "Esprit critique", "6": "Leadership", "7": "Empathie", "8": "Écoute active", "9": "Gestion du stress", "10": "Communication", "11": "Gestion du temps", "12": "Travail en équipe" },
-    specialite: { "1": "Sciences Mathématiques", "2": "Sciences de la Vie et de la Terre", "3": "Sciences Agronomiques", "4": "Sciences Physiques", "5": "Sciences et Technologies Électriques", "6": "Sciences et Technologies Mécaniques", "7": "Sciences Économiques", "8": "Sciences de Gestion Comptable", "9": "Lettres", "10": "Sciences Humaines" },
+    niveau_anglais: { "1": "Debutant", "2": "Intermediaire", "3": "Avance", "4": "Fluent" },
+    niveau_francais: { "1": "Debutant", "2": "Intermediaire", "3": "Avance", "4": "Fluent" },
+    matiere_detestee: { "0": "None", "1": "Mathematiques", "2": "Physique", "3": "Chimie", "4": "Litterature", "5": "Histoire", "6": "Langues", "7": "Informatique", "8": "Biologie" },
+    loisirs: { "1": "Lecture", "2": "Sport", "3": "Musique", "4": "Voyage", "5": "Cinema", "6": "Jeux video", "7": "Arts plastiques", "8": "Benevolat", "9": "Technologie", "10": "Ecriture", "11": "Photographie" },
+    matiere_preferee: { "1": "Mathematiques", "2": "Physique", "3": "Chimie", "4": "Litterature", "5": "Histoire", "6": "Langues", "7": "Informatique", "8": "Biologie" },
+    soft_skills: { "1": "Adaptabilite", "2": "Creativite", "3": "Resolution de problèmes", "4": "Autonomie", "5": "Esprit critique", "6": "Leadership", "7": "Empathie", "8": "ecoute active", "9": "Gestion du stress", "10": "Communication", "11": "Gestion du temps", "12": "Travail en equipe" },
+    specialite: { "1": "Sciences Mathematiques", "2": "Sciences de la Vie et de la Terre", "3": "Sciences Agronomiques", "4": "Sciences Physiques", "5": "Sciences et Technologies electriques", "6": "Sciences et Technologies Mecaniques", "7": "Sciences economiques", "8": "Sciences de Gestion Comptable", "9": "Lettres", "10": "Sciences Humaines" },
     ville: { 
         "0": "Casablanca", "1": "Rabat", "2": "Fes", "3": "Marrakech", "4": "Tangier", "5": "Agadir", "6": "Meknes", "7": "Oujda", "8": "Kenitra", "9": "Tetouan", "10": "Safi", 
         "11": "Mohammedia", "12": "El Jadida", "13": "Nador", "14": "Beni Mellal", "15": "Taza", "16": "Laayoune", "17": "Dakhla", "18": "Essaouira", "19": "Al Hoceima", 
@@ -39,7 +39,7 @@ const MAPPINGS = {
         "28": "Azrou", "29": "Ouarzazate", "30": "Tan-Tan", "31": "Guelmim", "32": "Smara", "33": "Larache", "34": "Midelt", "35": "Zagora", "36": "Chefchaouen", 
         "37": "Khouribga", "38": "El Kelaa des Sraghna", "39": "Berkane", "40": "Ifrane", "41": "Martil", "42": "Fnideq", "43": "Temara", "44": "Ait Melloul", 
         "45": "Ouazzane", "46": "Imzouren", "47": "Sefrou", "48": "Boujdour", "49": "Chichaoua", "50": "Azemmour", "51": "Aourir", "52": "Bir Jdid", 
-        "53": "Taroudant", "54": "Ait Ourir", "55": "Demnate", "56": "Oulad Teima", "57": "Skhirat", "58": "Tinghir", "59": "Bouarfa", "60": "Khémisset", 
+        "53": "Taroudant", "54": "Ait Ourir", "55": "Demnate", "56": "Oulad Teima", "57": "Skhirat", "58": "Tinghir", "59": "Bouarfa", "60": "Khemisset", 
         "61": "Jorf El Melha", "62": "Laayoune-Plage"
     }
 };
